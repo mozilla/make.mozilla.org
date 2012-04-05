019d7166882d75112365508686bc82e4ae0aa66c
Matt Patterson <matt@reprocessed.org>
2012-04-05 09:58:56 +0200

    Patch up a version of the PostGIS DB adapter which incorporates the changes from https://code.djangoproject.com/changeset/16826 so Django 1.3 / PostGIS / Postgres 9.1 play nice together

---
+++ make_mozilla/postgis/base.py
@@ -0,0 +1,36 @@
+from django.db.backends.postgresql_psycopg2.base import *
+from django.db.backends.postgresql_psycopg2.base import DatabaseWrapper as Psycopg2DatabaseWrapper
+from django.contrib.gis.db.backends.postgis.creation import PostGISCreation
+from django.contrib.gis.db.backends.postgis.introspection import PostGISIntrospection
+from django.contrib.gis.db.backends.postgis.operations import PostGISOperations
+from django.contrib.gis.db.backends.postgis.adapter import PostGISAdapter
+import psycopg2
+
+class PatchedPostGISAdapter(PostGISAdapter):
+    def __init__(self, geom):
+        super(PostGISAdapter, self).__init__(geom)
+        self._adapter = psycopg2.Binary
+
+    def prepare(self, conn):
+        """
+        This method allows escaping the binary in the style required by the
+        server's `standard_conforming_string` setting.
+        """
+        self._adapter.prepare(conn)
+
+    def getquoted(self):
+        "Returns a properly quoted string for use in PostgreSQL/PostGIS."
+        # psycopg will figure out whether to use E'\\000' or '\000'
+        return 'ST_GeomFromEWKB(%s)' % self._adapter.getquoted()
+
+class PatchedPostGISOperations(PostGISOperations):
+    Adapter = PostGISAdapter
+    Adaptor = Adapter # Backwards-compatibility alias.
+
+class DatabaseWrapper(Psycopg2DatabaseWrapper):
+    def __init__(self, *args, **kwargs):
+        super(DatabaseWrapper, self).__init__(*args, **kwargs)
+        self.creation = PostGISCreation(self)
+        self.ops = PatchedPostGISOperations(self)
+        self.introspection = PostGISIntrospection(self)
+
