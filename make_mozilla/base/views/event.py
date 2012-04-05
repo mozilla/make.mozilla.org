516b4c237d68b733e8ec49ccf83cf052d5352ea6
Matt Patterson <matt@reprocessed.org>
2012-04-05 10:01:38 +0200

    Wire up front to back for beyond-trivial event creation

--- make_mozilla/base/views/event.py
+++ make_mozilla/base/views/event.py
@@ -1,17 +1,18 @@
-import logging
-
 from django import http
-from django.shortcuts import render
-from django.shortcuts import render_to_response
 from django.forms import ModelForm
-from make_mozilla.base import models
-
+from django.core.urlresolvers import reverse
 
 import bleach
 import commonware
+import jingo
 from funfactory.log import log_cef
 from mobility.decorators import mobile_template
 from session_csrf import anonymous_csrf
+
+from make_mozilla.base import models
+
+
+from django.conf import settings
 
 log = commonware.log.getLogger('playdoh')
 
@@ -21,19 +22,22 @@ class EventForm(ModelForm):
 
 @anonymous_csrf
 def create(request):
+    print settings.DEFAULT_CHARSET
     if request.method == 'POST': # If the form has been submitted...
         form = EventForm(request.POST) # A form bound to the POST data
-        print form
         if form.is_valid(): # All validation rules pass
             # Process the data in form.cleaned_data
-            pass
-        return HttpResponseRedirect(url('event', {'event_id': 'x'})) # Redirect after POST
-    return render(request, 'root/index.html', data)
+            form.save()
+            return http.HttpResponseRedirect(
+                reverse('make_mozilla.base.views.event.detail', kwargs={'event_id': event.pk})
+            ) # Redirect after POST
+    return jingo.render(request, 'event/new.html', {'form': form})
 
 def detail(request, event_id):
-    return render_to_response('event/detail.html', {})
+    event = models.Event.objects.get(pk=event_id)
+    return jingo.render(request, 'event/detail.html', {'event': event})
 
 def new(request):
     new_event_form = EventForm()
-    return render(request, 'event/new.html', {'form': new_event_form})
+    return jingo.render(request, 'event/new.html', {'form': new_event_form})
 
