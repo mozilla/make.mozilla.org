8810a281c5e83d7d0cfbee7e18e3daf64f41d2f6
Matt Patterson <matt@reprocessed.org>
2012-04-12 18:26:06 +0200

    Also remove mention of Geography type from migration

--- make_mozilla/base/migrations/0001_initial.py
+++ make_mozilla/base/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(SchemaMigration):
             ('venue_address', self.gf('django.db.models.fields.TextField')()),
             ('country', self.gf('django.db.models.fields.CharField')(max_length=255)),
             ('event_url', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('location', self.gf('django.contrib.gis.db.models.fields.PointField')(blank=True, geography=True)),
+            ('location', self.gf('django.contrib.gis.db.models.fields.PointField')(blank=True)),
         ))
         db.send_create_signal('base', ['Event'])
 
@@ -30,7 +30,7 @@ class Migration(SchemaMigration):
             'country': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'event_url': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'location': ('django.contrib.gis.db.models.fields.PointField', [], {'blank': 'True', 'geography': 'True'}),
+            'location': ('django.contrib.gis.db.models.fields.PointField', [], {'blank': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'venue_address': ('django.db.models.fields.TextField', [], {}),
             'venue_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
