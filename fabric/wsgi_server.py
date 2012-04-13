cd59c20ddd4bf1b30476ad7db639b65c3211faae
Matt Patterson <matt@reprocessed.org>
2012-04-13 11:00:42 +0200

    touch playdoh.wsgi so that mod_wsgi reloads everything

--- fabric/wsgi_server.py
+++ fabric/wsgi_server.py
@@ -1,7 +1,9 @@
-from fabric.api import task
+from fabric.api import task, cd, run
+from release import latest_release_path
 
 @task
 def restart():
     """Currently a no-op until I figure out if mod_wsgi notices changes or not"""
-    pass
+    with cd(latest_release_path()):
+        run('touch wsgi/playdoh.wsgi')
 
