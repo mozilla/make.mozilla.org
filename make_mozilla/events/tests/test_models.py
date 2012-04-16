50cf94579d98490ad6c1a2defdca396df99408c0
Matt Patterson <matt@reprocessed.org>
2012-04-16 16:24:11 +0200

    Venue model changes to support setting lat/lon independently

--- make_mozilla/events/tests/test_models.py
+++ make_mozilla/events/tests/test_models.py
@@ -1,5 +1,4 @@
-import unittest
-from nose.tools import eq_, ok_
+from django.utils import unittest
 from django.contrib.gis import geos
 
 from make_mozilla.events import models
@@ -7,26 +6,26 @@ from make_mozilla.events import models
 class VenueTest(unittest.TestCase):
     def test_location_is_always_initialized_as_a_point_object(self):
         venue = models.Venue()
-        ok_(venue.location.wkt is not None)
+        self.assertIsNotNone(venue.location.wkt)
 
     def test_latitude_can_be_set(self):
         venue = models.Venue()
         venue.latitude = 51.456
 
-        eq_(venue.location, geos.Point(51.456, 0))
+        self.assertEqual(venue.location, geos.Point(51.456, 0))
     
     def test_latitude_can_be_gotten(self):
         venue = models.Venue(location = geos.Point(42, 24))
 
-        eq_(venue.latitude, 42.0)
+        self.assertEqual(venue.latitude, 42.0)
 
     def test_longitude_can_be_gotten(self):
         venue = models.Venue(location = geos.Point(42, 24))
 
-        eq_(venue.longitude, 24.0)
+        self.assertEqual(venue.longitude, 24.0)
 
     def test_longitude_can_be_set(self):
         venue = models.Venue()
         venue.longitude = 51.456
 
-        eq_(venue.location, geos.Point(0, 51.456))
+        self.assertEqual(venue.location, geos.Point(0, 51.456))
