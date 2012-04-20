0b2e92f8e15b319ea6fe846ef153c7d1e04de8a6
Ross Bruniges <rossbruniges@gmail.com>
2012-04-20 11:51:35 +0100

    Fixing fall-through on URLs.py by adding in '/' to the end of each, adding in settings required for the app to successfully deploy via fab

--- make_mozilla/events/urls.py
+++ make_mozilla/events/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import patterns, url
 from make_mozilla.events.views import events
 
 urlpatterns = patterns('',
-    url(r'^$',                 events.index,   name = 'events'),
-    url(r'^new$',              events.new,     name = 'event.new'),
-    url(r'^create$',           events.create,  name = 'event.create'),
-    url(r'^(?P<event_id>.+)$', events.details, name = 'event'),
+    url(r'^/$',                 events.index,   name = 'events'),
+    url(r'^new/$',              events.new,    name = 'event.new'),
+    url(r'^create/$',           events.create, name = 'event.create'),
+    url(r'^(?P<event_id>.+)/$', events.details, name = 'event'),
 )
