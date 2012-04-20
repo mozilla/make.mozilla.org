17fea2711a08f74b094e377b53759770aec83946
Matt Patterson <matt@reprocessed.org>
2012-04-20 20:10:49 +0200

    Failing test for event/venue data extraction from BSD api response [WIP].

--- make_mozilla/bsd/__init__.py
+++ make_mozilla/bsd/__init__.py
@@ -8,3 +8,13 @@ def parse_event_feed(feed_url):
 def process_events_json(events_json):
     return [event['id'] for event in events_json['results']]
 
+class BSDEventImporter(object):
+    event_extractors = []
+    venue_extractors = []
+
+    @classmethod
+    def extract_from_event_json(self, event_json):
+        pass
+
+    def __init__(self, event_feed_url):
+        self.event_feed_url = event_feed_url
--- make_mozilla/bsd/tests/test_bsd.py
+++ make_mozilla/bsd/tests/test_bsd.py
@@ -10,8 +10,8 @@ import json
 def fixture_path(path):
     return os.path.abspath(os.path.join(os.path.dirname(__file__), path))
 
-def json_fixture():
-    return json.load(open(fixture_path('fixtures/event_feed.json')))
+def json_fixture(fixture_name):
+    return json.load(open(fixture_path('fixtures/%s' % fixture_name)))
 
 class BSDEventFeedParserTest(unittest.TestCase):
     @patch.object(bsd, 'process_events_json')
@@ -32,11 +32,30 @@ class BSDEventFeedParserTest(unittest.TestCase):
         eq_(['bsd_api_id'], actual)
 
     def test_event_feed_parser_returns_6_events(self):
-        actual = bsd.process_events_json(json_fixture())
+        actual = bsd.process_events_json(json_fixture('event_feed.json'))
 
         eq_(6, len(actual))
 
     def test_event_feed_parser_returns_the_right_ids(self):
-        actual = bsd.process_events_json(json_fixture())
+        actual = bsd.process_events_json(json_fixture('event_feed.json'))
 
         eq_(['981', '983', '974', '971', '975', '936'], actual)
+
+class BSDEventImporterTest(unittest.TestCase):
+    @patch.object(bsd.BSDEventImporter, 'venue_extractors')
+    @patch.object(bsd.BSDEventImporter, 'event_extractors')
+    def test_relevant_information_is_extracted_from_event_json(self, mock_event_extractors, mock_venue_extractors):
+        event_json = json_fixture('event.json')
+        actual = bsd.BSDEventImporter.extract_from_event_json(event_json )
+        mock_event_extractor = Mock()
+        mock_venue_extractor = Mock()
+        mock_event_extractors.return_value = [mock_event_extractor]
+        mock_venue_extractors.return_value = [mock_venue_extractor]
+        mock_event_extractor.return_value = {1: 1}
+        mock_venue_extractor.return_value = {2: 2}
+
+        expected = {'event': {1: 1}, 'venue': {2: 2}}
+
+        mock_event_extractor.assert_called_with(event_json)
+        eq_(expected, actual)
+
