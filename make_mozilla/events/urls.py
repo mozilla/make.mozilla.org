cb31b23024a6a43fb8eb8a67fc82c7289c7649d4
Ross Bruniges <rossbruniges@gmail.com>
2012-04-20 12:04:18 +0100

    I am stooopid - put in the wrong URL

--- make_mozilla/events/urls.py
+++ make_mozilla/events/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, url
 from make_mozilla.events.views import events
 
 urlpatterns = patterns('',
-    url(r'^/$',                 events.index,   name = 'events'),
+    url(r'^$',                 events.index,   name = 'events'),
     url(r'^new/$',              events.new,    name = 'event.new'),
     url(r'^create/$',           events.create, name = 'event.create'),
     url(r'^(?P<event_id>.+)/$', events.details, name = 'event'),
