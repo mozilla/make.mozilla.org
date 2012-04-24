301b5d7ae4dca6f824e11926ab9082a60a911977
Matt Patterson <matt@reprocessed.org>
2012-04-24 17:49:27 +0200

    remove erroneous no-op comment from the wsgi_restart task.

--- fabric/wsgi_server.py
+++ fabric/wsgi_server.py
@@ -3,7 +3,6 @@ from release import latest_release_path
 
 @task
 def restart():
-    """Currently a no-op until I figure out if mod_wsgi notices changes or not"""
     with cd(latest_release_path()):
         run('touch wsgi/playdoh.wsgi')
 
