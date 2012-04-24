306cf5987c90d54d72037c19dd02f07be37cbb6f
Matt Patterson <matt@reprocessed.org>
2012-04-24 13:13:22 +0200

    Add integration test decorator to prevent certain tests running unless we really want them to.

--- make_mozilla/base/tests/decorators.py
+++ make_mozilla/base/tests/decorators.py
@@ -1,6 +1,7 @@
 from functools import wraps
 from nose.plugins.attrib import attr
 from nose.plugins.skip import SkipTest
+import os
 
 __all__ = ['wip']
 
@@ -16,4 +17,14 @@ def wip(f):
             raise SkipTest("WIP test failed: " + str(e))
         fail("test passed but marked as work in progress")
     return attr('wip')(run_test)
+
+def integration(f):
+    @wraps(f)
+    def run_test(*args, **kwargs):
+        integration_run = (os.getenv('INTEGRATION', None) is not None)
+        if integration_run:
+            f(*args, **kwargs)
+        else:
+            raise SkipTest("Skipping integration test")
+    return attr('integration')(run_test)
 
