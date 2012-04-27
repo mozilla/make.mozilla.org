9eb0890b34fd4a9298db47d7b5bb3cf1b8ed129b
Ross Bruniges <rossbruniges@gmail.com>
2012-04-27 13:12:39 +0100

    Merge from --rebase origin. Making PEP8 fixes along the way

--- make_mozilla/events/urls.py
+++ make_mozilla/events/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls.defaults import patterns, url
 from make_mozilla.events import views
 
 urlpatterns = patterns('',
-    url(r'^$',                  views.index,             name = 'events'),
-    url(r'^feed.rss$',          views.IndexGeoRSSFeed(), name = 'events-feed'),
-    url(r'^new/$',              views.new,               name = 'event.new'),
-    url(r'^create/$',           views.create,            name = 'event.create'),
-    url(r'^near/$',             views.near,              name = 'events.near'),
-    url(r'^near/map/$',         views.near_map,          name = 'events.near.map'),
-    url(r'^search/$',           views.search,            name = 'events.search'),
-    url(r'^(?P<event_id>.+)/$', views.details,           name = 'event'),
+    url(r'^$',                  views.index,             name='events'),
+    url(r'^guides/$',			views.guides_all,	     name='guides_all'),
+    url(r'^feed.rss$',          views.IndexGeoRSSFeed(), name='events-feed'),
+    url(r'^new/$',              views.new,               name='event.new'),
+    url(r'^create/$',           views.create,            name='event.create'),
+    url(r'^near/$',             views.near,              name='events.near'),
+    url(r'^near/map/$',        views.near_list,         name='events.near.list'),
+    url(r'^search/$',           views.search,            name='events.search'),
+    url(r'^(?P<event_id>.+)/$', views.details,           name='event'),
 )
--- make_mozilla/events/views.py
+++ make_mozilla/events/views.py
@@ -19,6 +19,7 @@ from make_mozilla.events import paginators
 
 log = commonware.log.getLogger('playdoh')
 
+
 # Workaround from http://gis.stackexchange.com/questions/7553/is-there-a-geodjango-tutorial-for-georssfeeds
 class IndexGeoRSSFeed(Feed):
     title = "Make Mozilla!"
@@ -30,13 +31,15 @@ class IndexGeoRSSFeed(Feed):
         return models.Event.upcoming()
 
     def item_extra_kwargs(self, item):
-        return {'geometry' : self.item_geometry(item)}
+        return {'geometry': self.item_geometry(item)}
 
     def item_geometry(self, item):
         return item.location
 
+
 def index(request):
-	return jingo.render(request, 'events/index.html')
+    return jingo.render(request, 'events/index.html')
+
 
 def new(request):
     new_event_form = forms.EventForm()
@@ -46,38 +49,44 @@ def new(request):
         'venue_form': new_venue_form
     })
 
+
 def search(request):
-	return jingo.render(request, 'events/search.html')
+    return jingo.render(request, 'events/search.html')
+
 
 def details(request, event_id):
-    event = models.Event.objects.get(pk = event_id)
+    event = models.Event.objects.get(pk=event_id)
     return jingo.render(request, 'events/detail.html', {'event': event})
+
 
 @require_POST
 def create(request):
     ef, vf = process_create_post_data(request.POST)
     if ef.is_valid() and vf.is_valid():
         event, venue = create_event_and_venue(ef, vf)
-        return http.HttpResponseRedirect(reverse('event', kwargs = {'event_id': event.id}))
+        return http.HttpResponseRedirect(reverse('event', kwargs={'event_id': event.id}))
     return jingo.render(request, 'events/new.html', {
         'event_form': ef,
         'venue_form': vf
     })
+
 
 def process_create_post_data(data):
     event_form = forms.EventForm(data)
     venue_form = forms.VenueForm(data)
     return (event_form, venue_form)
 
+
 def create_event_and_venue(event_form, venue_form):
     venue = venue_form.save()
-    event = event_form.save(commit = False)
+    event = event_form.save(commit=False)
     event.venue = venue
-    organiser_email = 'ross@mozillafoundation.org' # nasty temp hack while resolving some other email-related things
+    organiser_email = 'ross@mozillafoundation.org'  # nasty temp hack while resolving some other email-related things
     event.organiser_email = organiser_email
-    tasks.register_email_address_as_constituent.delay(organiser_email , '111')
+    tasks.register_email_address_as_constituent.delay(organiser_email, '111')
     event.save()
     return (event, venue)
+
 
 class Near(object):
     def extract_latlon(self, request):
@@ -90,7 +99,7 @@ class Near(object):
 
     def paginated_results(self, latitude, longitude, results_per_page, page):
         results = models.Event.near(latitude, longitude)
-        return paginators.results_page(results, results_per_page, page = page)
+        return paginators.results_page(results, results_per_page, page=page)
 
     def render(self, request, template, results_per_page):
         (lat, lon) = self.extract_latlon(request)
@@ -102,10 +111,20 @@ class Near(object):
 
 near_view = Near()
 
+
 @require_GET
 def near(request):
     return near_view.render(request, 'events/near-list.html', 24)
 
+
 @require_GET
 def near_map(request):
     return near_view.render(request, 'events/near-map.html', 4)
+
+
+def near_list(request):
+    return near_view.render(request, 'events/near-list.html', 4)
+
+
+def guides_all(request):
+    return jingo.render(request, 'events/guides/index.html', {})
