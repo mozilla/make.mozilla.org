3190ce846e57f5eed51489baf97a01dc8d6f2a7e
Matt Patterson <matt@reprocessed.org>
2012-04-30 17:33:55 +0200

    Puppet cron task definition to run the BSD Event importer every 10 minutes

--- puppet/manifests/classes/app.pp
+++ puppet/manifests/classes/app.pp
@@ -22,4 +22,10 @@ class app {
     onlyif => "test ! -e ${app_root}/virtualenv/bin/python",
     require => [Class['python'], File[$app_root], User[$app_user]];
   }
+
+  cron { "import_bsd_events":
+    command => "cd ${app_root}/current && ${app_root}/virtualenv/bin/python manage.py cron import_bsd_events",
+    user => $app_user,
+    minute => [5,15,25,35,45,55];
+  }
 }
