4e2cf621f45effc519cc2adc2e8580ded8ff9ee7
Matt Patterson <matt@reprocessed.org>
2012-05-02 20:02:48 +0200

    Allow specifying the post-browser-id landing page

--- make_mozilla/users/tests/test_views.py
+++ make_mozilla/users/tests/test_views.py
@@ -1,13 +1,23 @@
 from django.conf import settings
 from django.utils import unittest
 
+from django.test.client import Client, RequestFactory
 from mock import patch, Mock
 from nose.tools import eq_, ok_
 from make_mozilla.base.tests.assertions import assert_routing
 
 from make_mozilla.users import views
 
+rf = RequestFactory()
+
 class LoginJumpPageTest(unittest.TestCase):
     def test_that_it_routes(self):
         assert_routing('/users/login/', views.login, name = 'login')
 
+    @patch('jingo.render')
+    def test_that_redirect_url_is_correctly_inserted_into_the_context(self, mock_render):
+        request = rf.get('/users/login/', {'next': '/a/url'})
+
+        views.login(request)
+
+        mock_render.assert_called_with(request, 'login.html', {'post_verify_qs': '?next=/a/url'})
--- make_mozilla/users/views.py
+++ make_mozilla/users/views.py
@@ -6,11 +6,17 @@ from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from django.core.urlresolvers import reverse
 from django import http
+import urlparse, urllib
 import jingo
 
 from django_browserid.forms import BrowserIDForm
 
 from make_mozilla.users.models import UserProfile
 
+def _extract_redirect_destination(request):
+    if not request.GET.has_key('next'): return {'post_verify_qs': ''}
+    return {'post_verify_qs': '?next=' + urllib.quote_plus(urlparse.urlparse(request.GET['next']).path, safe = '/')}
+
+
 def login(request):
-    return jingo.render(request, 'login.html', {'user': request.user})
+    return jingo.render(request, 'login.html', _extract_redirect_destination(request))
