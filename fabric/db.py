d58a789eb99e21c8d35fb467ab8b190c74cc3c4e
Matt Patterson <matt@reprocessed.org>
2012-05-04 10:13:59 +0200

    Run syncdb before migrate in deploy_with_migrations

--- fabric/db.py
+++ fabric/db.py
@@ -1,10 +1,11 @@
-from fabric.api import task, cd, env, run
+from fabric.api import task, cd, env, run, execute
 from fabric.operations import sudo
 
 from release import latest_release_path, virtualenv_python_path
 
 @task
 def migrate():
+    execute(setup)
     with cd(latest_release_path()):
         run('%s manage.py migrate' % virtualenv_python_path())
 
