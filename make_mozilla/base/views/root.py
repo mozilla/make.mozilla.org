f348915340b0ae4c2c7749b2e998780088b4ffc5
Ross Bruniges <rossbruniges@gmail.com>
2012-05-04 09:22:59 +0100

    Quick add of error pages - will style these next

--- make_mozilla/base/views/root.py
+++ make_mozilla/base/views/root.py
@@ -1,6 +1,17 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 
+import jingo
+
+
 def index(request):
     return HttpResponseRedirect(reverse('events'))
+
+
+def fail(request):
+    return jingo.render(request, 'base/404.html', {}, status=404)
+
+
+def app_fail(request):
+    return jingo.render(request, 'base/500.html', {}, status=500)
 
