7bdc6dc6b3fbc80c3eafd87075319630ce52a082
Ross Bruniges <rossbruniges@gmail.com>
2012-05-07 08:15:58 +0100

    Adding in filters for the events to the django admin

--- make_mozilla/events/admin.py
+++ make_mozilla/events/admin.py
@@ -1,7 +1,15 @@
 from django.contrib.gis import admin
 from make_mozilla.events import models
 
-admin.site.register(models.Event)
+
+class EventAdmin(admin.ModelAdmin):
+    model = models.Event
+    list_display = ('name', 'kind', 'venue')
+    list_filter = ('kind', 'campaign', 'verified', 'official', 'public')
+
+
+admin.site.register(models.Event, EventAdmin)
 admin.site.register(models.EventKind)
 admin.site.register(models.Venue, admin.GeoModelAdmin)
 admin.site.register(models.Campaign)
+
