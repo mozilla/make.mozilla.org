d0f1184c74af63f6cb01c80f7c2820e67fa14365
Matt Patterson <matt@reprocessed.org>
2012-05-10 13:07:23 +0200

    PIL dependencies specified as -dev versions because it'll need to be installed through pip which won't see a site-packages version

--- puppet/manifests/classes/python.pp
+++ puppet/manifests/classes/python.pp
@@ -1,7 +1,15 @@
 # Install python
-class python {
+class pil_dependencies {
   package {
-    ["python2.6-dev", "python2.6", "libapache2-mod-wsgi", "python-wsgi-intercept", "python-pip", "python-virtualenv", "python-imaging"]:
+    ["libfreetype6-dev", "libjpeg62-dev"]:
       ensure => installed;
   }
+}
+
+class python {
+  package {
+    ["python2.6-dev", "python2.6", "libapache2-mod-wsgi", "python-wsgi-intercept", "python-pip", "python-virtualenv"]:
+      ensure => installed;
+  }
+  include pil_dependencies
 }
