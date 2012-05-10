85fe4f6566c417dd2ac26ba03b995adc2c18ac7f
Philippe M. Chiasson <gozer@mozilla.com>
2012-05-10 18:29:18 -0400

    Stick to Apache prefork

--- puppet/manifests/classes/apache.pp
+++ puppet/manifests/classes/apache.pp
@@ -1,5 +1,5 @@
 class apache {
-  package { "apache2-threaded-dev":
+  package { "apache2-prefork-dev":
     ensure => present,
     before => File['/etc/apache2/sites-available/playdoh'];
   }
@@ -9,7 +9,7 @@ class apache {
     owner => "root", group => "root", mode => 0644,
     notify  => Service['apache2'],
     require => [
-        Package['apache2-threaded-dev']
+        Package['apache2-prefork-dev']
     ];
   }
 
@@ -19,7 +19,7 @@ class apache {
     'a2enmod proxy':
       onlyif => 'test ! -e /etc/apache2/mods-enabled/proxy.load';
     'a2ensite playdoh': 
-      require => [Package['apache2-threaded-dev'], File['/etc/apache2/sites-available/playdoh']],
+      require => [Package['apache2-prefork-dev'], File['/etc/apache2/sites-available/playdoh']],
       onlyif => 'test ! -L /etc/apache2/sites-enabled/playdoh';
   }
 
@@ -27,7 +27,7 @@ class apache {
     ensure => running,
     enable => true,
     require => [
-      Package['apache2-threaded-dev'],
+      Package['apache2-prefork-dev'],
       File['/etc/apache2/sites-available/playdoh'],
       Exec['a2ensite playdoh']
     ];
