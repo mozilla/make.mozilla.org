7ce7186ecabce6617425fa3348abf089cd0325de
Andrew Hayward <github@andrewhayward.net>
2012-05-10 21:15:17 +0100

    Fixing pop-up positioning for map results pages with pagination

--- media/js/map.js
+++ media/js/map.js
@@ -246,7 +246,7 @@ var map = (function (config) {
 							    location = projection.fromLatLngToContainerPixel(marker.position),
 							    targetX = location.x + box.offsetWidth / 2 + (this.pixelOffset_.width||0),
 							    targetY = location.y - box.offsetHeight + (this.pixelOffset_.height||0) + (container.offsetHeight / 2)
-							        - $(search.form).offset().top - search.form.offsetHeight - 40,
+							        - $(search.form.parentNode).offset().top - search.form.parentNode.offsetHeight - 40,
 							    target = projection.fromContainerPixelToLatLng(new google.maps.Point(targetX, targetY));
 							
 							gmap.panTo(target);
