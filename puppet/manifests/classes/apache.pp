cb61252706f1fcd8579cec0f099cd4c2e2c6512c
Matt Patterson <matt@reprocessed.org>
2012-05-18 15:34:39 +0200

    Tweak apache puppet manifest to disable the correct site.

--- puppet/manifests/classes/apache.pp
+++ puppet/manifests/classes/apache.pp
@@ -18,8 +18,8 @@ class apache {
       onlyif => 'test ! -e /etc/apache2/mods-enabled/rewrite.load';
     'a2enmod proxy':
       onlyif => 'test ! -e /etc/apache2/mods-enabled/proxy.load';
-    'a2dissite playdoh':
-      onlyif => 'test -L /etc/apache2/sites-enabled/playdoh';
+    'a2dissite default && /etc/init.d/apache2 graceful':
+      onlyif => 'test -L /etc/apache2/sites-enabled/default';
     'a2ensite playdoh':
       require => [Package['apache2-prefork-dev'], File['/etc/apache2/sites-available/playdoh']],
       onlyif => 'test ! -L /etc/apache2/sites-enabled/playdoh';
