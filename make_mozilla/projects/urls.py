17dca5866884a7fc376559ef388deccb1b2757d6
Andrew Hayward <github@andrewhayward.net>
2012-05-23 23:17:27 +0100

    Setting up projects URLs

---
+++ make_mozilla/projects/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import patterns, url
+from make_mozilla.projects import views
+
+urlpatterns = patterns('',
+    url(r'^$',
+        views.index,                name='projects'),
+    url(r'(?P<slug>[\w-]+(?:-[\w-]+)*)/$',
+        views.details,              name='project'),
+)
