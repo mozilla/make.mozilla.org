d0b8873b99f2ca43c38a80dc62b8b2925d886e5a
Andrew Hayward <github@andrewhayward.net>
2012-06-01 16:26:50 +0100

    Adding new project models to admin

--- make_mozilla/projects/admin.py
+++ make_mozilla/projects/admin.py
@@ -2,4 +2,40 @@ from django.contrib.gis import admin
 from make_mozilla.projects import models
 
 
-admin.site.register(models.Project)
+class ProjectStepInline(admin.TabularInline):
+    model = models.ProjectStep
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'featured', )
+    list_editable = ('featured', )
+    list_filter = ('contributor', 'audience', 'tool', 'difficulty', 'skills', )
+    exclude = ('url_hash',)
+    # inlines = [ProjectStepInline,]
+
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'value':('label',),}
+
+
+class AudienceAdmin(TagAdmin):
+    pass
+
+
+class DifficultyAdmin(TagAdmin):
+    pass
+
+
+class SkillAdmin(TagAdmin):
+    pass
+
+
+class ContributorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(models.Project, ProjectAdmin)
+admin.site.register(models.Audience, AudienceAdmin)
+admin.site.register(models.Difficulty, DifficultyAdmin)
+admin.site.register(models.Skill, SkillAdmin)
+admin.site.register(models.Contributor, ContributorAdmin)
