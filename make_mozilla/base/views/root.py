756049db865769ab64b018deaf4d9b8bbacd24a6
Ross Bruniges <rossbruniges@gmail.com>
2012-06-05 13:59:50 +0100

    Adding in dynamic events to the homepage

--- make_mozilla/base/views/root.py
+++ make_mozilla/base/views/root.py
@@ -2,14 +2,17 @@ import jingo
 
 from make_mozilla.news.models import Article
 from make_mozilla.projects.models import Project
+from make_mozilla.events.models import Event
 
 
 def index(request):
     news = Article.objects.filter(featured=True).order_by('-updated')[0:3]
     projects = Project.objects.filter(featured=True).order_by('?')[0:5]
+    events = Event.objects.filter(official=True).order_by('start')[0:3]
     return jingo.render(request, 'splash.html', {
         'news': news,
         'projects': projects,
+        'events': events,
     })
 
 
