8a1f4988d42ffda6bce0d9f5e5b98b62901b77d7
Ross Bruniges <rossbruniges@gmail.com>
2012-06-05 13:29:47 +0100

    Adding in dynamic news articles to the homepage instead of the static

--- make_mozilla/base/views/root.py
+++ make_mozilla/base/views/root.py
@@ -1,8 +1,13 @@
 import jingo
 
+from make_mozilla.news.models import Article
+
 
 def index(request):
-    return jingo.render(request, 'splash.html', {})
+    news = Article.objects.filter(featured=True).order_by('-updated')[0:3]
+    return jingo.render(request, 'splash.html', {
+        'news': news,
+    })
 
 
 def fail(request):
