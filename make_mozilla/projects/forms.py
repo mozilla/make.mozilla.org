fba1f119b976101a099b1056f744fa253914f2eb
Andrew Hayward <github@andrewhayward.net>
2012-06-07 11:45:48 +0100

    Adjusting filter form for audience/topic switch

--- make_mozilla/projects/forms.py
+++ make_mozilla/projects/forms.py
@@ -1,23 +1,23 @@
 from django import forms
-from make_mozilla.projects.models import Audience, Difficulty, Skill
+from make_mozilla.projects.models import Topic, Difficulty, Skill
 from make_mozilla.tools.models import Tool
 
 class FilterForm(forms.Form):
-    audience = forms.ModelChoiceField(
+    difficulty = forms.ModelChoiceField(
         required=False,
-        queryset=Audience.objects.all(),
-        empty_label='audience',
+        queryset=Difficulty.objects.all(),
+        empty_label='difficulty',
+        to_field_name='value')
+    topic = forms.ModelChoiceField(
+        required=False,
+        queryset=Topic.objects.all(),
+        empty_label='topic',
         to_field_name='value')
     tool = forms.ModelChoiceField(
         required=False,
         queryset=Tool.objects.all(),
         empty_label='tool',
         to_field_name='slug')
-    difficulty = forms.ModelChoiceField(
-        required=False,
-        queryset=Difficulty.objects.all(),
-        empty_label='difficulty',
-        to_field_name='value')
     skill = forms.ModelChoiceField(
         required=False,
         queryset=Skill.objects.all(),
