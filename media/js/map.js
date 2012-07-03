2500ff4e13014ccd4f35502d12383ab6bc55cbd0
Ross Bruniges <rossbruniges@gmail.com>
2012-07-03 21:40:22 +0100

    Sorting out the date/time problem where events withouth an end date get imported as a date/time 1 minute BEFORE the start date/time

--- media/js/map.js
+++ media/js/map.js
@@ -421,18 +421,25 @@ var map = (function (config) {
                                     from_m = event.from.getMinutes(),
                                     from_time = (from_h % 12 === 0 ? 12 : from_h % 12) + ':' + (from_m < 10 ? '0' : '') + from_m + ' ' + (from_h < 12 ? 'AM' : 'PM'),
                                     from = from_date + ' ' + from_time,
+                                    from_epoc = new Date(from_date + ' ' + from_time).getTime(),
                                     to_ts = '',
                                     to_date = months[event.to.getMonth()] + ' ' + event.to.getDate() + ', ' + event.to.getFullYear(),
                                     to_h = event.to.getHours(),
                                     to_m = event.to.getMinutes(),
                                     to_time = (to_h % 12 === 0 ? 12 : to_h % 12) + ':' + (to_m < 10 ? '0' : '') + to_m + ' ' + (to_h < 12 ? 'AM' : 'PM'),
-                                    to = from_date === to_date ? to_time : to_date + ' ' + to_time;
+                                    to = from_date === to_date ? to_time : to_date + ' ' + to_time,
+                                    to_epoc = new Date(to_date + ' ' + to_time).getTime();
 
                                 // console.log(event.from, event.to);
 
                                 html.push('<li' + (event.official ? ' class="official"' : '') + '>');
                                 html.push('<a href="' + event.url + '" class="name">' + event.name + '</a>');
-                                html.push('<span class="date"><time class="from" datetime="' + from_ts + '">' + from + '</time><span> to </span><time class="to" datetime="' + to_ts + '">' + to + '</time></span>');
+                                html.push('<span class="date"><time class="from" datetime="' + from_ts + '">' + from + '</time>');
+                                // events without an end time get imported with an end time 1 minute BEFORE the start date...
+                                if (from_epoc < to_epoc) {
+                                        html.push('<span> to </span><time class="to" datetime="' + to_ts + '">' + to + '</time>');
+                                }
+                                html.push('</span>');
                                 html.push('<a href="' + event.type.url + '" class="type">' + event.type.name + '</a>');
                                 html.push('</li>');
                             }
