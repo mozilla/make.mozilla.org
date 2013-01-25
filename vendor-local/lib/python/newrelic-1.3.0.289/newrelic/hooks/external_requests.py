97e1338c5abca7c2bb1b5f3b763e1eedefdb52b4
Ross Bruniges <rossbruniges@gmail.com>
2013-01-25 11:14:35 +0000

    fixes#66 - removing new-relic from the local repo, this should be installed on the server

--- vendor-local/lib/python/newrelic-1.3.0.289/newrelic/hooks/external_requests.py
+++
@@ -1,9 +0,0 @@
-import newrelic.api.external_trace
-
-def instrument(module):
-
-    def url_request(method, url, *args, **kwargs):
-        return url
-
-    newrelic.api.external_trace.wrap_external_trace(
-           module, 'request', 'requests', url_request)
